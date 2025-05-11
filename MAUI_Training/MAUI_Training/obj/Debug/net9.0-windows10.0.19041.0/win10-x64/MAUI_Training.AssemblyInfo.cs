90ddd3eab05d2de9f45967ca85ce73d4e5dee758
Ubaid Khan <ubaid.khan@biamp.com>
2025-05-11 13:44:10 +0500

    second commit

--- MAUI_Training/MAUI_Training/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MAUI_Training.AssemblyInfo.cs
+++ MAUI_Training/MAUI_Training/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MAUI_Training.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAUI_Training")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+43ac730136d37b0c514b9afc76fa672f0dc5ed72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fa75530120312fb0e7d51141ff3b33ed7a804586")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAUI_Training")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAUI_Training")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
